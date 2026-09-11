11622968b3d23283c07b37c0c2f78997582d3143
DEEPIKASINGH608 <190765168+DEEPIKASINGH608@users.noreply.github.com.>
2026-09-11 21:33:36 +0530

    Fix CORS for Vercel deployments

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "https://freight-intelligence-system-2qyh7ooz5-deepikasingh608s-projects.vercel.app",
-                
+        "https://freight-intelligence-system-5kf4zw6hf-deepikasingh608s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
