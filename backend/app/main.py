a57458ccd880cfc7a6b59415f85e02b1958e8114
DEEPIKASINGH608 <190765168+DEEPIKASINGH608@users.noreply.github.com.>
2026-09-11 15:08:50 +0530

    Update port

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,6 +14,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://freight-intelligence-system-2qyh7ooz5-deepikasingh608s-projects.vercel.app",
+        "http://localhost:5174",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
     ],
     allow_credentials=True,
     allow_methods=["*"],
